c1f21fd5053777f36e361af538f336d502d133a6
Yossi Nivin <ynivin@gmail.com>
2018-05-01 23:12:07 +0200

    Wires and submodules are now stored in a dictionaries

--- Types/Module.py
+++ Types/Module.py
@@ -3,41 +3,37 @@ class Module(object):
     def __init__(self, name):
         self.name       = name
         self.parent     = None
-        self.submodules = []
-        self.wires      = []
+        self.submodules = {}
+        self.wires      = {}
 
     def add_submodule(self, new_submodule):
         new_submodule.set_parent(self)
-        self.submodules.append(new_submodule)
+        self.submodules[new_submodule.get_name()] = new_submodule
 
     def add_submodules(self, new_submodules):
         for module in new_submodules:
             module.set_parent(self)
-
-        self.submodules.extend(new_submodules)
+            self.submodules[module.get_name()] = module
 
     def add_wire(self, new_wire):
-        self.wires.append(new_wire)
+        self.wires[new_wire.get_name()] = new_wire
 
     def add_wires(self, new_wires):
-        self.wires.extend(new_wires)
-
-    def set_name(self, name):
-        self.name = name
+        self.wires.update({wire.get_name(): wire for wire in new_wires})
 
     def get_name(self):
         return self.name
 
     def get_fullname(self):
-        parent_fullname = parent.get_fullname() if parent else ""
+        parent_fullname = self.parent.get_fullname() if self.parent else ""
         return "%s/%s" % (parent_fullname, self.get_name())
 
     def tree(self, depth = 1, include_wires = True):
-        temp_tree = "%s%s%s\n"%(" "*4*(depth-1), "|===", self.name)
+        temp_tree = "%s%s%s\n"%(" "*4*(depth-1), "|===", self.get_name())
         if include_wires:
-            for wire in self.wires:
+            for wire in self.wires.values():
                 temp_tree += "%s%s%s\n"%(" "*4*(depth), "|---", wire.get_name())
-        for submodule in self.submodules:
+        for submodule in self.submodules.values():
             temp_tree += submodule.tree(depth+1)
         return temp_tree
 
